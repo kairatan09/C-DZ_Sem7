9d5b8afc19b4b26402394a1a58323d87cc9eff91
Kairat Nagmetov <kairatan09@gmail.com>
2023-03-05 01:19:54 +0500

    Task47.

--- Task-47/Program.cs
+++ Task-47/Program.cs
@@ -10,7 +10,7 @@ void FillArray(double[,] array)
   Random rnd = new Random();
   for (int i = 0; i < array.GetLength(0); i++)
     for (int j = 0; j < array.GetLength(1); j++)
-      array[i, j] = rnd.NextDouble();
+      array[i, j] = Math.Round(rnd.NextDouble(), 3, MidpointRounding.AwayFromZero);
 }
 
 void PrintArray(double[,] array)
@@ -18,7 +18,7 @@ void PrintArray(double[,] array)
   for (int i = 0; i < array.GetLength(0); i++)
   {
     for (int j = 0; j < array.GetLength(1); j++)
-      Console.Write($"{array[i, j]} \t");
+      Console.Write($"{array[i, j], 5} \t");
     Console.WriteLine();
   }
 }
